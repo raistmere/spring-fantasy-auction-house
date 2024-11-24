fd3b4a081ede4c00104902529e0cf547b25cdd5d
raistmere <raistmere@gmail.com>
2024-11-24 17:34:09 -0600

    feat: added spring boot starter package for project foundation

---
+++ src/main/java/com/raistmere/fantasyauctionhouse/FantasyAuctionHouseApplication.java
@@ -0,0 +1,13 @@
+package com.raistmere.fantasyauctionhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyAuctionHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyAuctionHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raistmere/fantasyauctionhouse/FantasyAuctionHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raistmere.fantasyauctionhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyAuctionHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
