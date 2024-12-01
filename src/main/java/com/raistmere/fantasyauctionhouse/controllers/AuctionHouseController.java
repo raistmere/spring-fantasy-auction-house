264bf59b892e7b65ad9f4a78f8f4184761e9c073
raistmere <raistmere@gmail.com>
2024-11-30 19:03:29 -0600

    fix: there was a problem where user can refresh the page and duplicate the auction post to the DB which created duplicates of the same item. Implemented the PRG pattern (post-redirect-get) to stop that from happening. Now when the user post a new auction it will redirect to the confirmation page with a get request.

--- src/main/java/com/raistmere/fantasyauctionhouse/controllers/AuctionHouseController.java
+++ src/main/java/com/raistmere/fantasyauctionhouse/controllers/AuctionHouseController.java
@@ -6,10 +6,7 @@ import com.raistmere.fantasyauctionhouse.services.InventoryItemServiceImpl;
 import com.raistmere.fantasyauctionhouse.services.ItemServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/auctionhouse")
@@ -33,12 +30,18 @@ public class AuctionHouseController {
         return "addauctionform";
     }
 
-    @PostMapping("/showconfirmaddauction")
+    @PostMapping("/postauction")
     public String postAddAuctionForm(@ModelAttribute("auctionItemId") Long auctionItemId, @ModelAttribute("auctionItemPrice") int auctionItemPrice) {
 
         Auction newAuction = new Auction(itemServiceImpl.findById(auctionItemId), auctionItemPrice);
 
         auctionServiceImpl.saveAuction(newAuction);
+
+        return "redirect:/auctionhouse/confirmaddauction";
+    }
+
+    @GetMapping("/confirmaddauction")
+    public String getConfirmAddAuction() {
 
         return "confirmaddauction";
     }
